dd5178b9dfabe1dae861acce1017b30bb71bd105
Максим Деханов <Maksim.dehanov@life.com.by>
2018-08-30 14:43:47 +0300

    Initial Commit

---
+++ DTF Feed Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DTF Feed Client
+//
+//  Created by Максим Деханов on 30.08.2018.
+//  Copyright © 2018 Максим Деханов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
