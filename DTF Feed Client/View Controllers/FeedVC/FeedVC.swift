08a1c256de01a0363cb69c18bbd68cb2a3af1958
Максим Деханов <Maksim.dehanov@life.com.by>
2018-08-30 15:00:44 +0300

    Added FeedVC

---
+++ DTF Feed Client/View Controllers/FeedVC/FeedVC.swift
@@ -0,0 +1,18 @@
+//
+//  FeedVC.swift
+//  DTF Feed Client
+//
+//  Created by Максим Деханов on 30.08.2018.
+//  Copyright © 2018 Максим Деханов. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+    
+    @IBOutlet weak var feedTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
